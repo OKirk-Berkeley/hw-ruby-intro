e73989e6fe87f5ae8f526ff6e5ee8a4cc8f7fd7d
OKirk-Berkeley <oliverkirk@berkeley.edu>
2026-09-02 09:53:39 +0000

    CHIPS 2.5 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,99 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  tot = 0
+  arr.each do |x|
+    tot = tot + x
+  end
+  tot
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+  big = -Float::INFINITY
+  big_jr = -Float::INFINITY
+  arr.each do |x|
+    if x > big
+      big_jr = big
+      big = x
+    elsif x > big_jr
+      big_jr = x
+    end
+  end
+  big + big_jr
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+  if arr.length < 2
+    return false
+  elsif arr.length == 2
+    return arr[0] + arr[1] == n
+  end
+  (1...arr.length).each do |i|
+    if arr[0] + arr[i] == n
+      return true
+    end
+  end
+  sum_to_n?(arr[1...arr.length], n)
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  nonletters = /[^a-zA-Z]/
+  vowels = /[aeiouAEIOU]/
+  if s.length == 0
+    return false
+  end
+  if (nonletters =~ s) == 0
+    return false
+  end
+  if (vowels =~ s) == 0
+    return false
+  end
+  true
 end
 
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  invalid = /[^01]/
+  if invalid.match(s)
+    return false
+  end
+  if s.length < 2 && s == '0'
+    return true
+  end
+  if s[s.length-2...s.length] == '00'
+    return true
+  end
+  false
 end
 
 # Part 3
 
 class BookInStock
-  # YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    if isbn == ''
+      raise ArgumentError, "isbn can't be empty"
+    end
+    if price <= 0.0
+      raise ArgumentError, "price can't be less than or equal to 0"
+    end
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string()
+    sprintf('$%.2f', @price)
+  end
 end
